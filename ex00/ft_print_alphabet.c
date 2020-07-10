76c409e66f2e926281ee1092de5d90787694262a
Corban Loots <coloots@c5r11s10.wethinkcode.co.za>
2020-07-10 09:09:50 +0200

    Complete.

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,18 +6,21 @@
 /*   By: coloots <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 16:45:26 by coloots           #+#    #+#             */
-/*   Updated: 2020/07/09 08:47:30 by coloots          ###   ########.fr       */
+/*   Updated: 2020/07/10 09:04:30 by coloots          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-	 void ft_print_alphabet(void)
-	{
-		char letter;
+void	ft_putchar(char c);
 
-		letter = 'a';
-		while (letter<= 'z')
-		{
-			ft_putchar(letter);
-			letter++;
-			}
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		++letter;
 	}
+}
+
